32cd5da74e22a98040632ab3c130d6c56fcd9e49
Thomas Heinze <thomas.heinze@dhge.de>
2023-01-15 20:11:04 +0100

    fixed error

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 cd BigCloneEval/commands
+./deleteTool -t=1
 ./registerTool -n="StoneDetector" -d "StoneDetector"
 ./detectClones -m=100000 -o="clones.csv" -r="../../BCE_runner"
 ./clearClones -t=1
 ./importClones -c="clones.csv" -t=1
-./evaluateTool -t=1 -mil=15 -o="../../BigCloneEval_Report.txt"
+./evaluateTool -t=1 --mil=15 -o="../../BigCloneEval_Report.txt"
