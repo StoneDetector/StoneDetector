459c400b6f8d3c52548bf5ff229c9550fd36dfd6
Thomas  Heinze <t.heinze@mailbox.org>
2023-01-15 18:12:42 +0000

    Adjust -m=100000 and -mil=15 in run_benchmark.sh

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,7 +2,7 @@
 
 cd BigCloneEval/commands
 ./registerTool -n="StoneDetector" -d "StoneDetector"
-./detectClones -m=10000 -o="clones.csv" -r="../../BCE_runner"
+./detectClones -m=100000 -o="clones.csv" -r="../../BCE_runner"
 ./clearClones -t=1
 ./importClones -c="clones.csv" -t=1
-./evaluateTool -t=1 -o="../../BigCloneEval_Report.txt"
+./evaluateTool -t=1 -mil=15 -o="../../BigCloneEval_Report.txt"
